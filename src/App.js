7134c67381ed72d417c6ceb9959510ceedc18e1d
Jihan Shamas <jihan.shamas@gmail.com>
2023-03-16 09:55:55 +0200

    login

--- src/App.js
+++ src/App.js
@@ -28,41 +28,37 @@
 //   );
 // }
 // App.js
-import React, { useState, useEffect } from 'react';
-import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, Route, Routes as Routess, useLocation } from "react-router-dom";
 import "./App.css";
-import Login from './Pages/Login/Login';
+import Login from "./Pages/Login/Login";
 // import Register from './Pages/Login/Register';
-import Profile from './Pages/Admin_Panel/Profile';
-import Routes from './Components/Routes'
+import Profile from "./Pages/Admin_Panel/Profile";
+import Routes from "./Components/Routes";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname === '/') {
+    if (location.pathname === "/") {
       setLoggedIn(false);
     }
   }, [location]);
 
   return (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/login" />
-      </Route>
-      <Route path="/login">
+    <Routess>
+      {/* <Route exact path="/">
+        <Link to="/login" />
+      </Route> */}
+      {/* <Route path="/login">
         <Login setLoggedIn={setLoggedIn} />
-      </Route>
-      <Routes>
-        <Route path="/profile">
-          <Profile />      
-        </Route>
-      </Routes>
-    </Switch>
+      </Route> */}
+      <Route path="/" element={<Login setLoggedIn={setLoggedIn} />} />
+      <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routess>
   );
 };
 
 export default App;
-
-
